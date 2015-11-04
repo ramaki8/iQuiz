25c001263eceea750559247097871a0c0a6d8c0f
Rachel Kipps <rachelkipps@gmail.com>
2015-11-03 22:11:02 -0800

    completed part 1

--- iQuiz/ViewController.swift
+++ iQuiz/ViewController.swift
@@ -8,16 +8,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    let subjects = ["Mathematics", "Marvel Super Heroes", "Science"]
+    let imageNames = ["math.png", "marvel.png", "science.png"]
+    let descriptions = ["Add, subtract, multiply and divide.", "Up, up and away!", "Test your scientific knowledge."]
 
+    
+    @IBOutlet weak var quizTable: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        quizTable.dataSource = self
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.subjects.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = self.quizTable.dequeueReusableCellWithIdentifier("CategoryCell")! as UITableViewCell
+        let row = indexPath.row
+        
+        let subjectLabel = cell.viewWithTag(10) as! UILabel
+        subjectLabel.text = subjects[row]
+        
+        let subjectDescription = cell.viewWithTag(20) as! UILabel
+        subjectDescription.text = descriptions[row]
+        
+        let img = cell.viewWithTag(30) as! UIImageView
+        img.image = UIImage(named: imageNames[row])
+        
+        return cell
+    }
+    
+    @IBAction func pressSettings(sender: UIBarButtonItem) {
+        let alertController = UIAlertController(title: "Settings", message: "Settings go here", preferredStyle: UIAlertControllerStyle.Alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+        
+        self.presentViewController(alertController, animated: true, completion: nil)
     }
 
 
